4fecd2c123ba35c70e7db8f2a879d93474ef900e
jonahnzhang <jonahnzhang@tencent.com>
2022-08-30 08:25:15 +0800

    learn on gorutin

--- Go/factory/main.go
+++ Go/factory/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	ak47, _ := getGun("ak47")
@@ -8,6 +11,69 @@ func main() {
 
 	printDetails(ak47)
 	printDetails(musket)
+
+	// var quit = make(chan bool)
+
+	// Serve(clientRequests, quit)
+	// Serve(clientRequests, quit)
+
+	go SendReq([]int{1, 2, 3})
+	go SendReq([]int{4, 5, 6})
+	go SendReq([]int{7, 8, 9})
+
+	// }
+}
+
+func SendReq(args []int) <-chan *Request {
+	var clientRequests = make(chan *Request)
+	go func() {
+		request := &Request{args, sum, make(chan int)}
+		// Send request
+		clientRequests <- request
+
+		// Wait for response.
+		fmt.Printf("answer: %d\n", <-request.resultChan)
+	}()
+
+	return clientRequests
+}
+
+var MaxOutstanding int = 3
+
+// var sem = make(chan int, MaxOutstanding)
+
+// func process() {
+// 	time.Sleep(time.Second * 1)
+// }
+
+type Request struct {
+	args       []int
+	f          func([]int) int
+	resultChan chan int
+}
+
+func sum(a []int) (s int) {
+	for _, v := range a {
+		s += v
+	}
+	time.Sleep(time.Second * 1)
+	return
+}
+
+func handle(queue chan *Request) {
+	// time.Sleep(time.Second * 1)
+	for req := range queue {
+		req.resultChan <- req.f(req.args)
+	}
+}
+
+func Serve(clientRequests chan *Request, quit chan bool) {
+	// Start handlers
+	for i := 0; i < MaxOutstanding; i++ {
+		go handle(clientRequests)
+	}
+	<-quit // Wait to be told to exit.
+
 }
 
 func printDetails(g IGun) {
